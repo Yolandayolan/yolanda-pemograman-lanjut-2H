5cf50e6e90c45fb1e0c03bda09ec6518b2fc3296
yolanda <12050120358@students.uin-suska.ac.id>
2021-03-26 16:17:08 +1400

    basic konsep

---
+++ conditionals and loops/com/yolanda/conditionals/and/loops/DesicionMaking.java
@@ -0,0 +1,12 @@
+package com.yolanda.conditionals.and.loops;
+
+public class DesicionMaking {
+    public static void main(String[] args) {
+        int x = 5;
+
+        if ( x == 5 ) {
+            System.out.println("Yeah");
+
+        }
+    }
+}
