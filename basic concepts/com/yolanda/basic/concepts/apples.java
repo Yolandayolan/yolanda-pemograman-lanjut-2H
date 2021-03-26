4d964c036ceecc359330ca3aa98586411017d43c
yolanda <12050120358@students.uin-suska.ac.id>
2021-03-26 15:37:00 +1400

    basic konsep

---
+++ basic concepts/com/yolanda/basic/concepts/apples.java
@@ -0,0 +1,12 @@
+package com.yolanda.basic.concepts;
+
+public class apples {
+    public static void main(String[] args) {
+        {
+
+            System.out.println("Hello, World!");
+
+        }
+
+    }
+    }
